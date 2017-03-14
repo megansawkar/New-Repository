7a726e6287a2b22e33c1924d64e7bb57469cbd3d
megansawkar <megan.sawkar@gmail.com>
2017-03-14 09:46:03 -0700

    Master up to date

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,12 +1,15 @@
 (function() {
 		function SongPlayer() {
 				var SongPlayer = {};
-			
+				/**
+				* @desc Currently playing song audio file
+				* @type {Object}
+				*/
 				var currentSong = null;
-			 /**
-			 * @desc Buzz object audio file
-			 * @type {Object}
-			 */
+			  /**
+			  * @desc Buzz object audio file
+			  * @type {Object}
+			  */
      		var currentBuzzObject = null;
 					
 				/**
@@ -27,23 +30,38 @@
 
 						currentSong = song;
 				};
-			
+				
+				/**
+				* @function playSong
+				* @desc Sets currently playing song to true and sets current Buzz {Object} to play
+				* @param {Object} song
+				*/
 				var playSong = function(song) {
 						currentBuzzObject.play();
 						song.playing = true;
 				};
 				
+				/**
+				* @function SongPlayer.play
+				* @desc Sets the clicked song and plays the clicked song
+				* @param {Object} song
+				*/
 				SongPlayer.play = function(song) {
 						if (currentSong !== song) {
 								setSong(song);
 								playSong(song);
 						} else if (currentSong === song) {
 								if (currentBuzzObject.isPaused()) {
-										currentBuzzObject.play();
+										playSong(song);
 								}
 						}
 				};
 			
+				/**
+				* @function SongPlayer.pause
+				* @desc Sets the currently playing song to false and the Buzz object to pause
+				* @param {Object} song
+				*/
 				SongPlayer.pause = function(song) {
 				currentBuzzObject.pause();
 				song.playing = false;
