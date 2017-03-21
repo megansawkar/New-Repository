542bba1fa6cecaefbdcc7353a2c60d5989d3130f
megansawkar <megan.sawkar@gmail.com>
2017-03-20 21:12:35 -0700

    Assignment 11: Complete

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -1,23 +1,25 @@
 (function() {
-		function timecode() {
+	function timecode() {
 				return function(seconds) {
 						var seconds = Number.parseFloat(seconds);
 					
-						if (Number.isNaN(seconds)) {
-								return '-:--';
-						}
+						var output = buzz.toTimer(seconds);
 					
-						var wholeSeconds = Math.floor(seconds);
-						var minutes = Math.floor(wholeSeconds / 60);
-						var remainingSeconds = wholeSeconds % 60;
-
-						var output = minutes + ':';
-
-						if (remainingSeconds < 10) {
-								output += '0';   
-						}
-
-						output += remainingSeconds;
+//						if (Number.isNaN(seconds)) {
+//								return '-:--';
+//						}
+//					
+//						var wholeSeconds = Math.floor(seconds);
+//						var minutes = Math.floor(wholeSeconds / 60);
+//						var remainingSeconds = wholeSeconds % 60;
+//
+//						var output = minutes + ':';
+//
+//						if (remainingSeconds < 10) {
+//								output += '0';   
+//						}
+//
+//						output += remainingSeconds;
 
 						return output;
 				};
